f8dc691e74aead60806f9c5b13dac3a89d57e27d
salamalala <hello@ninaregli.ch>
2015-03-13 13:15:09 +0000

    remove unused  javascript code

--- js/boxlayout.js
+++ js/boxlayout.js
@@ -2,20 +2,8 @@ var Boxlayout = (function() {
 
   var $element = $( '#block-main' ),
   $sections = $element.children( 'section:not(:first)' ),
-    // works section
-    $sectionWork = $( '#block-work-section' ),
-    // work items
-    $workItems = $( '#block-work-items > li' ),
-    // work panels
-    $workPanelsContainer = $( '#block-panel-work-items' ),
-    $workPanels = $workPanelsContainer.children( 'div' ),
-    totalWorkPanels = $workPanels.length,
-    // navigating the work panels
-    $nextWorkItem = $workPanelsContainer.find( 'nav > span.block-next-work' ),
-    // if currently navigating the work items
-    isAnimating = false,
-    // close work panel trigger
-    $closeWorkItem = $workPanelsContainer.find( 'nav > span.block-icon-close' ),
+   
+    
     transEndEventNames = {
       'WebkitTransition' : 'webkitTransitionEnd',
       'MozTransition' : 'transitionend',
@@ -67,34 +55,6 @@ var Boxlayout = (function() {
     } );
 
 
-    // navigating the work items: current work panel scales down and the next work panel slides up
-    $nextWorkItem.on( 'click', function( event ) {
-
-      if( isAnimating ) {
-        return false;
-      }
-      isAnimating = true;
-
-      var $currentPanel = $workPanels.eq( currentWorkPanel );
-      currentWorkPanel = currentWorkPanel < totalWorkPanels - 1 ? currentWorkPanel + 1 : 0;
-      var $nextPanel = $workPanels.eq( currentWorkPanel );
-
-      $currentPanel.removeClass( 'block-show-work' ).addClass( 'block-hide-current-work' ).on( transEndEventName, function( event ) {
-        if( !$( event.target ).is( 'div' ) ) return false;
-        $( this ).off( transEndEventName ).removeClass( 'block-hide-current-work' );
-        isAnimating = false;
-      } );
-
-      if( !supportTransitions ) {
-        $currentPanel.removeClass( 'block-hide-current-work' );
-        isAnimating = false;
-      }
-      
-      $nextPanel.addClass( 'block-show-work' );
-
-      return false;
-
-    } );
 
     // clicking the work panels close button: the current work panel slides down and the section scales up again
     $closeWorkItem.on( 'click', function( event ) {
