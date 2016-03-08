18c273f5c8b9cf37d09d0fcd964280e8f7b0f8a4
salamalala <hello@ninaregli.ch>
2016-03-08 10:16:30 +0000

    feat(*): run jekyll and sass with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,79 @@
-module.exports = function(grunt){
+'use strict';
 
-  grunt.initConfig({
-    
-    sass: {
-      options: {
-        sourceMap: true
-      },
-      dist: {
-        files: {
-            'css/main.css': 'css/sass/main.scss'
-        }
-      }
-    },
+module.exports = function (grunt) {
 
-    watch:{
-      sass:{
-        files:['**/*.scss'],
-        tasks: ['sass'],
-      }
-    }
-  });
+    // Show elapsed time after tasks run to visualize performance
+    require('time-grunt')(grunt);
+    // Load all Grunt tasks that are listed in package.json automagically
+    require('load-grunt-tasks')(grunt);
 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-  // Default Task
-  
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-sass');
+        // shell commands for use in Grunt tasks
+        shell: {
+            jekyllBuild: {
+                command: 'jekyll build'
+            },
+            jekyllServe: {
+                command: 'jekyll serve'
+            }
+        },
 
+        // watch for files to change and run tasks when they do
+        watch: {
+            sass: {
+                files: ['_sass/**/*.{scss,sass}'],
+                tasks: ['sass']
+            }
+        },
 
+        // sass (libsass) config
+        sass: {
+            options: {
+                sourceMap: true,
+                relativeAssets: false,
+                outputStyle: 'expanded',
+                sassDir: '_sass',
+                cssDir: '_site/css'
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: '_sass/',
+                    src: ['**/*.{scss,sass}'],
+                    dest: '_site/css',
+                    ext: '.css'
+                }]
+            }
+        },
 
-  grunt.registerTask('default', [
-    'sass',
-    'watch'
+        // run tasks in parallel
+        concurrent: {
+            serve: [
+                'sass',
+                'watch',
+                'shell:jekyllServe'
+            ],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+
+    });
+
+    // Register the grunt serve task
+    grunt.registerTask('serve', [
+        'concurrent:serve'
     ]);
-};
 
+    // Register the grunt build task
+    grunt.registerTask('build', [
+        'shell:jekyllBuild',
+        'sass'
+    ]);
+
+    // Register build as the default task fallback
+    grunt.registerTask('default', 'build');
+
+};
