b72682f01dc0b564e85e6e9807f84506f33b8e67
salamalala <hello@ninaregli.ch>
2016-03-14 11:01:31 +0000

    feat(animation): scroll work title and about title with changing colors

--- _js/main.js
+++ _js/main.js
@@ -4,19 +4,34 @@ $(function() {
   var scrollMagicController = new ScrollMagic.Controller();
   
   // Create Animation for 0.5s
-  // var tween = TweenMax.to('#work--title', 3, {
-  //   backgroundColor: 'rgb(255, 39, 46)',
-  //   scale: 5,
-  //   rotation: 360
-  // });
+  var tween1 = TweenMax.to('#work--title', 3, {
+    backgroundColor: 'rgb(51, 188, 154)',
+    delay: 2.99
+  });
   
   // Create the Scene and trigger when visible
-  var scene = new ScrollMagic.Scene({
+  var scene1 = new ScrollMagic.Scene({
     triggerElement: '#work', 
     duration: "150%" 
   })
   // .setTween(tween)
   .setPin("#work--title" , {pushFollowers: false})
+  .setTween(tween1)
+  .addTo(scrollMagicController);
+
+  // Create Animation for 0.5s
+  var tween2 = TweenMax.to('#about-me--title', 2, {
+    backgroundColor: 'rgb(101, 117, 116)'
+  });
+  
+  // Create the Scene and trigger when visible
+  var scene2 = new ScrollMagic.Scene({
+    triggerElement: '#about-me--title', 
+    duration: "150%" 
+  })
+  // .setTween(tween)
+  .setPin("#about-me--title" , {pushFollowers: false})
+  .setTween(tween2)
   .addTo(scrollMagicController);
   
 });
